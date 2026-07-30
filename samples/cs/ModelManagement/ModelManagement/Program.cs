f802f48087f3d1c878d54fe352e096febe7e25ef
Philippe Laval <philippe.laval@qualiparc.net>
2026-07-30 22:53:18 +0200

    add classes

--- samples/cs/ModelManagement/ModelManagement/Program.cs
+++ samples/cs/ModelManagement/ModelManagement/Program.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using Microsoft.AI.Foundry.Local;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
+using ModelManagement.Interfaces;
 using Serilog;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ModelManagement;
 
@@ -14,6 +17,12 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
+        var _loggerFactory = Microsoft.Extensions.Logging.LoggerFactory.Create(builder =>
+        {
+            builder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Information);
+        });
+        var _logger = _loggerFactory.CreateLogger("ModelManagement");
+
         // Bootstrapping Serilog from appsettings.json explicitly so logging is active
         var configuration = new ConfigurationBuilder()
             .SetBasePath(AppContext.BaseDirectory)
@@ -28,6 +37,12 @@ internal class Program
 
         try
         {
+            // Initialize the manager first (see Quick Start)
+            await FoundryLocalManager.CreateAsync(
+                new Configuration { AppName = "my-app" },
+                _logger);
+            var mgr = FoundryLocalManager.Instance;
+
             Log.Information("Starting host");
 
             var builder = Host.CreateDefaultBuilder(args)
@@ -39,6 +54,13 @@ internal class Program
                 .UseSerilog() // use the static Log.Logger we created
                 .ConfigureServices((context, services) =>
                 {
+                    // Register the FoundryLocalManager singleton instance created earlier
+                    services.AddSingleton(FoundryLocalManager.Instance);
+
+                    // Register EPManagement for IEPManagement so it can be injected
+                    services.AddSingleton<IEPManagement, EPManagement>();
+                    services.AddScoped<ICatalogManagement, CatalogManagement>();
+
                     services.AddHostedService<Worker>();
                 });
 
